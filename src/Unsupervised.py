bf142502f927d73735c130940312fc6e007f4057
Nikhil Gupta <nikhil97gupta@gmail.com>
2017-02-22 22:43:03 -0800

    added parameters for generation

--- src/Unsupervised.py
+++ src/Unsupervised.py
@@ -28,27 +28,27 @@ def unsupervised_learning(n_states, n_iters):
 
     return HMM, obs_map
 
-def sequence_generator(HMM, obs_map, k, M):
+def sequence_generator(HMM, obs_map, n, l, w):
     '''
     Arguments:
         HMM:        Trained HMM.
-        K:          Number of sequences to generate.
-        M:          Length of emission to generate.
+        n:          Number of poems to generate.
+        l:          Number of lines in each poem.
+        w:          Number of words in each poem.
     '''
     print()
     print()
     # Generate k input sequences.
-    for i in range(k):
-        # Generate a single input sequence of length m.
-        x = HMM.generate_emission(M)
-        for obs in x:
-            print(obs_map[obs], end=' ')
+    for _ in range(n):
+        for i in range(l):
+            # Generate a single input sequence of length m.
+            x = HMM.generate_emission(w)
+            for obs in x:
+                print(obs_map[obs], end=' ')
+            print()
         print()
 
-        # Print the results.
-        # print("{:30}".format(x))
-
 if __name__ == '__main__':
-    HMM, obs_map = unsupervised_learning(10, 1)
-    sequence_generator(HMM, obs_map, 14, 10)
+    HMM, obs_map = unsupervised_learning(10, 100)
+    sequence_generator(HMM, obs_map, 5, 14, 10)
 
